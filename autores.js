58909c2daf26d7ff693a4d32e4b9469d5460251c
David Gómez <167591175+Tossy06@users.noreply.github.com>
2025-08-11 16:26:50 -0500

    Update autores.js

--- autores.js
+++ autores.js
@@ -1,1 +1,25 @@
+const repoOwner = "Tossy06";
+const repoName = "Bolsa-De-Empleo";
+
+async function cargarAutores() {
+  const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`);
+  const autores = await response.json();
+
+  const contenedor = document.getElementById("autores-cards");
+
+  autores.slice(0, 3).forEach(autor => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+      <img src="${autor.avatar_url}" alt="${autor.login}">
+      <h3>${autor.login}</h3>
+      <a href="${autor.html_url}" target="_blank">Ver perfil</a>
+    `;
+
+    contenedor.appendChild(card);
+  });
+}
+
+cargarAutores();
 
