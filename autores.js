b600f1686022e16b449dbdf31a84b08bc6b3e123
David Gómez <167591175+Tossy06@users.noreply.github.com>
2025-08-22 06:53:30 -0500

    Update autores.js

--- autores.js
+++ autores.js
@@ -5,14 +5,14 @@ async function cargarAutores() {
   const contenedor = document.getElementById("autores-cards");
 
   // Autor principal fijo
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.innerHTML = `
+  const autor-card = document.createElement("div");
+  autor-card.classList.add("autor-card");
+  autor-card.innerHTML = `
     <img src="https://github.com/${repoOwner}.png" alt="${repoOwner}">
     <h3>${repoOwner}</h3>
     <a href="https://github.com/${repoOwner}" target="_blank">Ver perfil</a>
   `;
-  contenedor.appendChild(card);
+  contenedor.appendChild(autor-card);
 
   // Intentar traer colaboradores
   const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`);
@@ -21,14 +21,14 @@ async function cargarAutores() {
   autores.forEach(autor => {
     if (autor.login !== repoOwner) { // evitar duplicar
       console.log("Autor!!!", autor);
-      const card = document.createElement("div");
-      card.classList.add("card");
-      card.innerHTML = `
+      const autor-card = document.createElement("div");
+      autor-card.classList.add("autor-card");
+      autor-card.innerHTML = `
         <img src="${autor.avatar_url}" alt="${autor.login}">
         <h3>${autor.login}</h3>
         <a href="${autor.html_url}" target="_blank">Ver perfil</a>
       `;
-      contenedor.appendChild(card);
+      contenedor.appendChild(autor-card);
     }
   });
 }
